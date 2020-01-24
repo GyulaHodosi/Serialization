539e5dfdd494230439048c7c591c4171485e5540
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-01-24 20:55:28 +0100

    Implement Person class

---
+++ SerializePeople/SerializePeople/SerializePeople/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializePeople
+{
+    enum Genders { Male, Female}
+    class Person
+    {
+        string Name { get; set; }
+        DateTime BirthDate { get; set; }
+        Genders Gender { get; set; }
+        int Age { get; set; }
+
+        public Person(string name, DateTime birthDate, Genders gender)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            Gender = gender;
+            int now = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+            int dob = int.Parse(birthDate.ToString("yyyyMMdd"));
+            Age = (now - dob) / 10000;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nGender: {Gender}\nBirthDate: {BirthDate}\nAge: {Age}";
+        }
+    }
+}
---
+++ SerializePeople/SerializePeople/SerializePeople/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializePeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bela = new Person("Bela", new DateTime(1991, 06, 19), Genders.Male);
+            Console.WriteLine(bela.ToString());
+            Console.ReadKey();
+        }
+    }
+}
